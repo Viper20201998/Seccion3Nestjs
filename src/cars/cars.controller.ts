9c57dbb9bdf726c770ce2498e2a12d7707a93226
Viper20201998 <lesterasmethsandoval@gmail.com>
2024-06-22 12:37:52 -0600

    Seccion 3 asta injeccion de dependencias

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+  //como tal el id es un string para convertirlo en nuemero se puede usar el +id o el metodo Number(id)
+  //tenemos un problema con este metodo cuando se manda un strin y pueden venir letras y siempre devuelve un estatus 200
+  //Solucion habria que validar que pase solo numeros que si no es un numero que no lo envie.
+  //pero si los id estan compuestos por hash habria que validar en el otro metodo cars.service.
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    return this.carsService.findOneById(+id);
+  }
+}
